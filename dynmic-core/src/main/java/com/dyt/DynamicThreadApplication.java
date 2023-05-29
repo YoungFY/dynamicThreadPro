4f711d0f07d5434f5cfabbbd75d5119820e835be
YoungFY <1195850979@163.com>
2023-05-29 22:48:11 +0800

    Initial commit

---
+++ dynmic-core/src/main/java/com/dyt/DynamicThreadApplication.java
@@ -0,0 +1,16 @@
+package com.dyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * @Author wenLong
+ * @Description
+ * @Date 2023-05-29 22:47
+ **/
+@SpringBootApplication
+public class DynamicThreadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicThreadApplication.class, args);
+    }
+}
---
+++ dynmic-core/src/main/java/com/dyt/controller/DynamicThreadController.java
@@ -0,0 +1,18 @@
+package com.dyt.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * @Author wenLong
+ * @Description
+ * @Date 2023-05-29 22:45
+ **/
+@Slf4j
+@RestController
+@RequestMapping("/dyt/thread")
+@RequiredArgsConstructor
+public class DynamicThreadController {
+}
